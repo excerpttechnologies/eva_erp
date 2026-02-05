ff3ecef78c66f4a6c77a3bb74f92621d5ff73f94
Sreexcerpt <sree.excerpt@gmail.com>
2026-02-05 17:24:18 +0530

    updated dist

--- dist/js/src/util/scrollbar.js
+++ dist/js/src/util/scrollbar.js
@@ -31,7 +31,7 @@ class ScrollBarHelper {
 
     // Public
     getWidth() {
-        // https://developer.mozilla.org/en-US/docs/Web/API/Window/innerWidth#usage_notes
+        // https://developer.mozilla.org/en-US/docs/Web/api/Window/innerWidth#usage_notes
         const documentWidth = document.documentElement.clientWidth
         return Math.abs(window.innerWidth - documentWidth)
     }
